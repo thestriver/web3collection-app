d36f27035d6d35596ea899bf2412acbb4741332c
thestriver <wannabeamumin@gmail.com>
2022-05-31 17:10:00 +0100

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,14 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className='min-h-[100vh]' style={{ background: '#181C2A'}}>
        <Head>
         <title>Web3 Collection</title>
         <meta name="description" content="The biggest directory of tools and resources for web3 developers and dev-entrepreneurs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </>
+
+    </div>
   )
 }
