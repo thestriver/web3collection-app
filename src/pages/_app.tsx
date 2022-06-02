e6b1962beebf7f61dec0830de6de49404432bdb0
thestriver <wannabeamumin@gmail.com>
2022-06-02 02:40:00 +0100

    _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DataProvider } from '../utils/dataContext'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+     <DataProvider>
+      <Component {...pageProps} />
+    </DataProvider>
+    </>
+  )
 }
+
+export default MyApp
