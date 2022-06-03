c32a8ef15e362cf33d8180f75e933d7fa51a3fe5
thestriver <wannabeamumin@gmail.com>
2022-06-03 20:25:00 +0100

    nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,170 @@
+import Link from "next/link";
+import { useState } from "react";
+
+export const Nav = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+    return (
+      <div className="">
+        <div className="px-4 py-5 sm:max-w-xl md:max-w-full lg:max-w-screen-xl">
+          <div className="relative flex items-center justify-between">
+            <Link
+              href="/"
+              aria-label="Company"
+              title="Company"
+              className="inline-flex items-center"
+            >
+              <svg
+                className="w-8 text-textblack"
+                viewBox="0 0 24 24"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                stroke="currentColor"
+                fill="none"
+              >
+                <rect x="3" y="1" width="7" height="12" />
+                <rect x="3" y="17" width="7" height="6" />
+                <rect x="14" y="1" width="7" height="6" />
+                <rect x="14" y="11" width="7" height="12" />
+              </svg>
+              <span className="ml-2 text-xl font-bold tracking-wide text-transparent bg-clip-text bg-gradient-to-br from-green-500 via-teal-500 to-pink-500">
+                Web3Collection
+              </span>
+            </Link>
+            <ul className="xl:flex md:hidden items-center hidden space-x-10 lg:flex float-right">
+            <li>
+            </li>
+              <li>
+                 <a
+                  href="https://twitter.com/codevore1"
+                  aria-label="Our product"
+                  title="Our product"
+                  target='_blank' rel='noreferrer'
+                  className="font-medium tracking-wide text-textblack transition-colors duration-200 hover:text-white"
+                >
+                <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" viewBox="0 0 248 204" className="w-6 text-textblack">
+                    <path fill="#1d9bf0" d="M221.95 51.29c.15 2.17.15 4.34.15 6.53 0 66.73-50.8 143.69-143.69 143.69v-.04c-27.44.04-54.31-7.82-77.41-22.64 3.99.48 8 .72 12.02.73 22.74.02 44.83-7.61 62.72-21.66-21.61-.41-40.56-14.5-47.18-35.07 7.57 1.46 15.37 1.16 22.8-.87-23.56-4.76-40.51-25.46-40.51-49.5v-.64c7.02 3.91 14.88 6.08 22.92 6.32C11.58 63.31 4.74 33.79 18.14 10.71c25.64 31.55 63.47 50.73 104.08 52.76-4.07-17.54 1.49-35.92 14.61-48.25 20.34-19.12 52.33-18.14 71.45 2.19 11.31-2.23 22.15-6.38 32.07-12.26-3.77 11.69-11.66 21.62-22.2 27.93 10.01-1.18 19.79-3.86 29-7.95-6.78 10.16-15.32 19.01-25.2 26.16z"/>
+                </svg>
+
+                </a>
+                </li>
+                <li>
+                <Link
+                  href="/submit"
+                  aria-label="Our product"
+                  title="Our product"
+                  className="font-medium text-sm tracking-wide text-textblack transition-colors duration-200 hover:text-white"
+                >
+                  Contact or Submit A Resource
+                </Link>
+              </li>
+            </ul>
+            <div className="lg:hidden">
+              <button
+                aria-label="Open Menu"
+                title="Open Menu"
+                className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+                onClick={() => setIsMenuOpen(true)}
+              >
+                <svg className="w-5 text-textblack" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                  />
+                  <path
+                    fill="currentColor"
+                    d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                  />
+                  <path
+                    fill="currentColor"
+                    d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                  />
+                </svg>
+              </button>
+              {isMenuOpen && (
+                <div className="absolute top-0 left-0 w-full">
+                  <div className="p-5 bg-backblack border rounded shadow-sm">
+                    <div className="flex items-center justify-between mb-4">
+                      <div>
+                        <Link
+                          href="/"
+                          aria-label="Homepage"
+                          title="Homepage"
+                          className="inline-flex items-center"
+                        >
+                          <svg
+                            className="w-8 text-deep-purple-accent-400"
+                            viewBox="0 0 24 24"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeMiterlimit="10"
+                            stroke="currentColor"
+                            fill="none"
+                          >
+                            <rect x="3" y="1" width="7" height="12" />
+                            <rect x="3" y="17" width="7" height="6" />
+                            <rect x="14" y="1" width="7" height="6" />
+                            <rect x="14" y="11" width="7" height="12" />
+                          </svg>
+                          <span className="ml-2 text-xl font-bold tracking-wide text-transparent bg-clip-text bg-gradient-to-br from-green-500 via-teal-500 to-pink-500 uppercase">
+                            Web3Collection
+                          </span>
+                        </Link>
+                      </div>
+                      <div>
+                        <button
+                          aria-label="Close Menu"
+                          title="Close Menu"
+                          className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                          onClick={() => setIsMenuOpen(false)}
+                        >
+                          <svg className="w-5 text-textblack" viewBox="0 0 24 24">
+                            <path
+                              fill="currentColor"
+                              d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                    <nav>
+                      <ul className="space-y-4">
+                      <li>
+                        <Link
+                        href="/"
+                        aria-label="Our product"
+                        title="Our product"
+                        className="font-medium tracking-wide text-textblack transition-colors duration-200 hover:text-white"
+                        >
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" viewBox="0 0 248 204" className="w-6 text-textblack">
+                            <path fill="#1d9bf0" d="M221.95 51.29c.15 2.17.15 4.34.15 6.53 0 66.73-50.8 143.69-143.69 143.69v-.04c-27.44.04-54.31-7.82-77.41-22.64 3.99.48 8 .72 12.02.73 22.74.02 44.83-7.61 62.72-21.66-21.61-.41-40.56-14.5-47.18-35.07 7.57 1.46 15.37 1.16 22.8-.87-23.56-4.76-40.51-25.46-40.51-49.5v-.64c7.02 3.91 14.88 6.08 22.92 6.32C11.58 63.31 4.74 33.79 18.14 10.71c25.64 31.55 63.47 50.73 104.08 52.76-4.07-17.54 1.49-35.92 14.61-48.25 20.34-19.12 52.33-18.14 71.45 2.19 11.31-2.23 22.15-6.38 32.07-12.26-3.77 11.69-11.66 21.62-22.2 27.93 10.01-1.18 19.79-3.86 29-7.95-6.78 10.16-15.32 19.01-25.2 26.16z"/>
+                        </svg>
+
+                        </Link>
+                        </li>
+                        <li>
+                          <Link
+                            href="/"
+                            aria-label="Submit A Resource"
+                            title="Submit A Resource"
+                            className="font-medium tracking-wide text-textblack transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          >
+                            Submit A Resource
+                          </Link>
+                        </li>
+                        <li>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
