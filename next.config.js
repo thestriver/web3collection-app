71a37731215530c2d1765428ede88175033cc1d1
thestriver <wannabeamumin@gmail.com>
2022-06-07 22:10:00 +0100

    setu

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['dl.airtable.com', 'v5.airtableusercontent.com', 'pbs.twimg.com'],
+  },
 }
 
 module.exports = nextConfig
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import Hero from "./Hero";
 import { Nav } from "./Nav";
 import SearchBar from "./SearchBar";
 import Buttons from './Buttons'
+import SideBar from './SideBar'
 
 export {
     Hero,
     SearchBar,
     Nav,
-    Buttons
+    Buttons,
+    SideBar
 }
